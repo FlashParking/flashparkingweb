d72cec34173cd3042bf5c46d04987b1ee13b27eb
Nishantha <nishantha95@hotmail.fr>
2016-03-24 14:30:41 +0100

    new
    plus de base de donnée - j’ai supprimer les fichiers dessus et j’ai
    modifier HomeController, donc sa ne fait qu’appeller le twig que j’ai
    fait

--- src/AppBundle/Controller/Backend/HomeController.php
+++ src/AppBundle/Controller/Backend/HomeController.php
@@ -24,47 +24,4 @@ class HomeController extends Controller
             'nav_active' => 'search',
         ));
     }
-
-    /**
-     * @Route("/default", name="default")
-     */
-    public function indexAction()
-    {
-
-        $em = $this->getDoctrine()->getManager();
-        $description = $em->getRepository('AppBundle:Description')->findAll();
-        $partenaire = $em->getRepository('AppBundle:Partenaire')->findAll();
-
-        dump ($description[0]);
-
-        // replace this example code with whatever you need
-        return $this->render('@App/front/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-            'nav_active' => 'search',
-            'description' => $description,
-            'partenaire' => $partenaire,
-        ));
-    }   
-
-        public function formAction(Request $request)
-        {
-            // On initialise notre objet Desk
-            $desk = new Desk();
-
-            // On créé l'objet form à partir du formBuilder (En passant en param l'objet Desk)
-            $form = $this->createFormBuilder($desk)
-                ->add('title', 'text') // On ajoute le champ titre dans un input text
-                ->add('summary', 'textarea') // Idem pour le résumé mais dans un champ textarea
-                ->add('description', 'textarea') // Idem pour description
-                ->getForm(); // On récupère l'objet form 
-
-            if ($request->getMethod() == 'POST') { // Si on a soumis le formulaire
-                $form->bindRequest($request); // On bind les valeurs du POST à notre formulaire
-
-                if ($form->isValid()) { 
-
-                }
-            }
-
-        }
 }
